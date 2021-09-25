8240da57061b5f19303a90612f35c53ee7b31c5a
Joe Castrigno <jcastrigno@gmail.com>
2021-09-25 14:08:57 -0600

    added tests

--- assessment.js
+++ assessment.js
@@ -1,41 +1,3 @@
-/*
-Problem:
-Write a method that takes a string as an input and converts it to an integer.
-Do not use any type of built-in methods like int.Parse or atoi.
-Remember it's always good to include both positive and negative test cases.
-Error handling of your choice should be done in some way.
-
-Remember to add appropriate comments to explain what you are doing.
-
-Input: string
-
-Return: integer
-
-Example input:
-"baseball"
-"forty-two"
-"repeat after me"
-"What in the heck!?"
-"EWOXN-9300161-QUX"
-"--]{]''?><<>,.//f;s/fa/sfy"
-"#($_!+#$)@$%*%"
-"hey\ndude\nwhat's\nup?"
-" a b cdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=_+\`|~,./<>?\;'[]:"{}n"
-
-Data structure(s):
-Object that correlates a number to a string value
-
-Algorithm:
-Take string input
-split it into individual characters
-lookup the number value for the character
---if the character doesn't exist in the library
-----use 99 as the number value
---add the number value to the return string
-convert the return string to an integer and return
-
-*/
-
 const alphabet = {
   " ": "0",
   a: "1",
@@ -173,4 +135,8 @@ function convertStrToInt(str) {
   checkSafetyOfValue(Number(result));
 
   return Number(result);
+}
+
+module.exports = {
+  convertStrToInt
 }
--- tests/assessment.test.js
+++ tests/assessment.test.js
@@ -1,17 +1,61 @@
+const assessmentImport = require("../assessment.js");
 
+const { convertStrToInt } = assessmentImport;
 
 // Succeed:
-// console.log(convertStrToInt("93"));
-// console.log(convertStrToInt("baseball"));
-// console.log(convertStrToInt("forty-two"));
-// console.log(convertStrToInt("repeat after me"));
-// console.log(convertStrToInt("What in the heck!?"));
-// console.log(convertStrToInt("EWOXN-9300161-QUX"));
-// console.log(convertStrToInt("--]{]''?><<>,.//f;s/fa/sfy"));
-// console.log(convertStrToInt("#($_!+#$)@$%*%"));
-// console.log(convertStrToInt("hey\ndude\nwhat's\nup?"));
+
+test('convertStrToInt("93") === "6155"', () => {
+  expect(convertStrToInt("93")).toBe(6155);
+});
+
+test('convertStrToInt("baseball") === "21195211212"', () => {
+  expect(convertStrToInt("baseball")).toBe(21195211212);
+});
+
+test('convertStrToInt("forty-two") === "61518202563202310"', () => {
+  expect(convertStrToInt("forty-two")).toBe(61518202563202310);
+});
+
+test('convertStrToInt("repeat after me") === "185165120016205200000"', () => {
+  expect(convertStrToInt("repeat after me")).toBe(185165120016205200000);
+});
+
+test('convertStrToInt("hey\ndude\nwhat\'s\nup?") === "8.525954214595238e+30"', () => {
+  expect(convertStrToInt("hey\ndude\nwhat's\nup?")).toBe(8.525954214595238e+30);
+});
+
+test('convertStrToInt("EWOXN-9300161-QUX") === "3.1494150406361556e+33"', () => {
+  expect(convertStrToInt("EWOXN-9300161-QUX")).toBe(3.1494150406361556e+33);
+});
+
+test('convertStrToInt("--]{]\'\'?><<>,.//f;s/fa/sfy") === "6.363838283797977e+47"', () => {
+  expect(convertStrToInt("--]{]\'\'?><<>,.//f;s/fa/sfy")).toBe(6.363838283797977e+47);
+});
+
+test('convertStrToInt(""#($_!+#$)@$%*%"") === "8.793886485668788e+29"', () => {
+  expect(convertStrToInt("#($_!+#$)@$%*%")).toBe(8.793886485668788e+29);
+});
+
+test('convertStrToInt("What in the heck!??") === "4.981200914020851e+24"', () => {
+  expect(convertStrToInt("What in the heck!?")).toBe(4.981200914020851e+24);
+});
+
 
 // Fail:
-// console.log(convertStrToInt(["error array!"]));
-// console.log(convertStrToInt({error: "an object!"}));
-// console.log(convertStrToInt(42));
+
+test('convertStrToInt(["error array!"]) to throw error"', () => {
+  expect(() => {
+    convertStrToInt(["error array!"])
+  }).toThrow();
+});
+
+test('convertStrToInt({error: "an object!"}) to throw error"', () => {
+  expect(() => {
+    convertStrToInt({error: "an object!"})
+  }).toThrow();
+});
+test('convertStrToInt(42) to throw error"', () => {
+  expect(() => {
+    convertStrToInt(42)
+  }).toThrow();
+});
